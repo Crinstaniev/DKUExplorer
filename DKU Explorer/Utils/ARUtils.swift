1ad93c26442f5c5e20426a1e2ce1d6451d7dcd63
Johnathan Zhuang <zhuangzesen@gmail.com>
2023-03-14 03:30:52 +0800

    update object recognition

--- DKU Explorer/Utils/ARUtils.swift
+++ DKU Explorer/Utils/ARUtils.swift
@@ -9,3 +9,31 @@ import Foundation
 import RealityKit
 import ARKit
 
+
+func saveImageToPhotoLibrary(image: CIImage) {
+    // Convert the CIImage to a UIImage
+    let context = CIContext()
+    guard let cgImage = context.createCGImage(image, from: image.extent) else {
+        fatalError("Failed to create CGImage from CIImage")
+    }
+    let uiImage = UIImage(cgImage: cgImage)
+
+    // Save the UIImage to the photo library
+    UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+}
+
+extension CGRect {
+    func aspectFill(to bounds: CGRect) -> CGRect {
+        let aspect = self.width / self.height
+        let targetAspect = bounds.width / bounds.height
+        if aspect > targetAspect {
+            let targetWidth = self.height * targetAspect
+            let x = (self.width - targetWidth) / 2.0
+            return CGRect(x: x, y: self.origin.y, width: targetWidth, height: self.height)
+        } else {
+            let targetHeight = self.width / targetAspect
+            let y = (self.height - targetHeight) / 2.0
+            return CGRect(x: self.origin.x, y: y, width: self.width, height: targetHeight)
+        }
+    }
+}
--- DKU Explorer/Views/HomeARView.swift
+++ DKU Explorer/Views/HomeARView.swift
@@ -20,7 +20,6 @@ struct HomeARView: View {
 class ObjectDetector: ObservableObject {
     // debug use only
     private init() {
-        
     }
     
     static let shared = ObjectDetector()
@@ -34,6 +33,9 @@ class ObjectDetector: ObservableObject {
     @Published var boundingBox: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
     @Published var currentCIImage: CIImage = CIImage()
     
+    var frameWidth: CGFloat = 0
+    var frameHeight: CGFloat = 0
+    
     var timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {t in updateView(t)})
     
     func setRecognizedObject(_ newObjectName: String) {
@@ -45,14 +47,16 @@ class ObjectDetector: ObservableObject {
     }
     
     func getObjectCentralPoint() -> CGPoint {
-        // TODO: find the central point of the bounding box then return
-        return CGPoint(x: 0, y: 0)
+        let x = self.boundingBox.midX
+        let y = self.boundingBox.midY
+        return CGPoint(x: x, y: y)
     }
     
     func setCurrentImage(_ currentCIImage: CIImage) {
         self.currentCIImage = currentCIImage
     }
 }
+
 
 // this function fired periodically
 func updateView(_ timer: Timer) {
@@ -64,6 +68,26 @@ func updateView(_ timer: Timer) {
     let session = arview.session
     let model = recogd.model
     
+    // Get the current ARFrame
+    guard let currentFrame = arview.session.currentFrame else { return }
+    
+    // Get the camera image size in pixels
+    let imageResolution = currentFrame.camera.imageResolution
+    let imageSize = CGSize(width: CGFloat(imageResolution.width), height: CGFloat(imageResolution.height))
+    
+    // Compute the rect for the CIImage
+    let imageRect = CGRect(origin: .zero, size: imageSize)
+        .aspectFill(to: arview.bounds) // Use an extension method to fit the rect inside the bounds
+    
+    // Get the camera image as a CVPixelBuffer
+    let pixelBuffer = currentFrame.capturedImage
+    
+    // Convert the CVPixelBuffer to a CIImage
+    let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        .cropped(to: imageRect) // Crop the CIImage to the computed rect
+    
+    saveImageToPhotoLibrary(image: ciImage)
+    
     // capture image on the current frame
     let currentImageBuffer: CVImageBuffer? = session.currentFrame?.capturedImage
     if currentImageBuffer == nil {
@@ -73,6 +97,12 @@ func updateView(_ timer: Timer) {
     
     let currentCIImage = CIImage(cvImageBuffer: currentImageBuffer!)
     
+    recogd.frameWidth = currentCIImage.extent.width
+    recogd.frameHeight = currentCIImage.extent.height
+    
+    print("frameWidth: \(recogd.frameWidth)")
+    print("frameHeight: \(recogd.frameHeight)")
+    
     let request = VNCoreMLRequest(model: model) {
         (request, error) in
         guard let results = request.results as? [VNRecognizedObjectObservation],
@@ -80,26 +110,11 @@ func updateView(_ timer: Timer) {
             fatalError("Failed to perform object detection: \(error!.localizedDescription)")
         }
         
-        if results.count == 0 {
-            print("the result is empty")
-            return
-        }
-        
-        // handle results
-        let boundingBox = results[0].boundingBox
-        let label = results[0].labels[0]
-        let confidence = label.confidence
-        let identifier = label.identifier
-        
-        print("BoundingBox: \(boundingBox)")
-        print("identifier: \(identifier)")
-        print("confidence: \(confidence)")
+        recognitionResultHandler(results)
     }
     
     let inputSize = CGSize(width: 416, height: 416)
     let scaledImage = currentCIImage.transformed(by: CGAffineTransform(scaleX: inputSize.width / currentCIImage.extent.width, y: inputSize.height / currentCIImage.extent.height))
-    
-    recogd.setCurrentImage(scaledImage)
     
     let imageRequestHandler = VNImageRequestHandler(ciImage: scaledImage, orientation: .right, options: [:])
     
@@ -108,19 +123,32 @@ func updateView(_ timer: Timer) {
     } catch {
         fatalError("Failed to perform object detection: \(error.localizedDescription)")
     }
+}
+
+func recognitionResultHandler(_ results: [VNRecognizedObjectObservation]) {
+    // Handle the result
+    @ObservedObject var recogd = ObjectDetector.shared
+
+    // return if no observation
+    if results.count == 0 {
+        print("No object detected")
+        return
+    }
     
-    // TODO: filter observations (confidence based)
+    let observation = results[0]
+    let objectBound = VNImageRectForNormalizedRect(observation.boundingBox, Int(recogd.frameWidth), Int(recogd.frameHeight))
+    let label = observation.labels[0].identifier
+    let confidence = observation.confidence
     
-    // we only handle 1 object at a time
-    //    let topLabelObservation = observations[0].identifier
-    //    let firstWord = topLabelObservation.components(separatedBy: [","])[0]
-    //    let topLabelInfo = observations[0]
-    //
-    //    if recogd.recognizedObject != firstWord {
-    //        DispatchQueue.main.async {
-    //            recogd.setRecognizedObject(firstWord)
-    //        }
-    //    }
+    // return if confidence < 0.5
+    if Float(confidence) < 0.5 {
+        print("observation not confidence enough: \(confidence)")
+        return
+    }
+    
+    print("objectBound: \(objectBound)")
+    print("label: \(label)")
+    print("confidence: \(confidence)")
 }
 
 struct ARViewContainer: UIViewRepresentable {
@@ -134,9 +162,16 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {
         let arview = recogd.arview
-        let session = arview.session
+        // testing raycast
+        let screenCenter = CGPoint(x: arview.bounds.midX, y: arview.bounds.midY)
+//        let ray = arview.raycast(from: screenCenter, allowing: ARRaycastQuery.Target.existingPlaneGeometry, alignment: ARRaycastQuery.TargetAlignment.any)
         
+        let ray = arview.raycast(from: screenCenter, allowing: .estimatedPlane, alignment: .any)
         
+        print("casting from \(arview.bounds.midX), \(arview.bounds.midY)")
+        print(arview.bounds.height)
+        print(arview.bounds.width)
+        print("ray: \(ray)")
     }
     
 }
